aed97fdccbccb53e6c1694cda301a3cd7f9849da
TEW_Thammarat <84270286+tew080@users.noreply.github.com>
2026-02-10 20:51:03 +0700

    logdebug_service: Fix Warning

--- lib/services/logdebug_service.dart
+++ lib/services/logdebug_service.dart
@@ -3,6 +3,6 @@ import 'package:flutter/foundation.dart';
 void log(String massag) {
   // จะทำงานเมื่ออยู่ในหมด debug เท่านั้น (kDebugMode)
   if (kDebugMode) {
-    debugPrint("[LOG DEBUG]: ${massag}");
+    debugPrint("[LOG DEBUG]: $massag");
   }
 }
