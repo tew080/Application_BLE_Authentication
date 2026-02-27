60a8ecb24a3e9c03070fd784fae4180425c5b712
TEW_Thammarat <84270286+tew080@users.noreply.github.com>
2026-02-27 15:19:27 +0700

    authentication_service: Clean code and Update logic

--- lib/services/authentication_service.dart
+++ lib/services/authentication_service.dart
@@ -1,36 +1,31 @@
 // นำเข้า FirestoreService เพื่อดึงข้อมูล User
 import 'firestore_service.dart';
-
 // นำเข้าไลบรารี Flutter Secure Storage เพื่อจัดเก็บข้อมูลในเครื่อง
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
 // นำเข้า LogdebugService
 import '../services/logdebug_service.dart';
 
 class AuthenticationService {
-  // สร้าง FlutterSecureStorage เพื่อจัดเก็บข้อมูลในเครื่อง
+  //เข้ารหัสข้อมูลที่บันทึกในเครื่อง encryptedSharedPreferences = true (เพื่อให้อ่านข้อมูลที่เข้ารหัสได้)
   static const storage = FlutterSecureStorage(
-    // encryptedSharedPreferences = true เพื่อเข้ารหัสข้อมูลที่จัดเก็บในเครื่อง
     aOptions: AndroidOptions(encryptedSharedPreferences: true),
   );
   // ฟังก์ชันสำหรับเข้าสู่ระบบ (Login)
   // รับค่า studentId และ password เข้ามา
   // คืนค่าเป็น Future<bool> (จริง/เท็จ)
-  //static Future<bool> login(String studentId, String password) async {
   static Future<bool> login(String studentId) async {
     // สร้าง Instance ของ FirestoreService เพื่อใช้งาน
     final FirestoreService firestoreService = FirestoreService();
-
     // เรียกดึงข้อมูล User จาก Firestore ตาม studentId
     final doc = await firestoreService.getUser(studentId);
-
     // ตรวจสอบว่ามีเอกสาร (Document) นี้อยู่ในฐานข้อมูลหรือไม่
-    if (!doc.exists) {
+    if (!doc.exists || doc['loginStatus'] == true) {
       // ถ้าไม่มีข้อมูล ให้คืนค่า false (Login ไม่สำเร็จ)
       return false;
     } else {
       // ถ้ามีข้อมูล ให้ บันทึกข้อมูลลง Storage และคืนค่า true (Login สำเร็จ)
-      await storage.write(key: 'my_student_id', value: studentId);
+      await storage.write(key: 'student_id', value: studentId);
+      await FirestoreService().updateUser(studentId, {'loginStatus': true});
       log("LOGIN studentId='$studentId'");
       return true;
     }
