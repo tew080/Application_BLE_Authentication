43122492a03e29c4ab4a9810d3e8496c3c020840
TEW_Thammarat <84270286+tew080@users.noreply.github.com>
2026-08-04 17:44:59 +0700

    update

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     RSSI_THRESHOLD = -66  # ระยะประมาณ 1 เมตร
     UNLOCK_DELAY = 3
 
-    FB_KEY_PATH = "key/studentdata-37c33-firebase-adminsdk-fbsvc-aa56be95e5.json"
+    FB_KEY_PATH = "key/studentdata-37c33-firebase-adminsdk-fbsvc-a64a59d6e7.json"
     COLLECTION_STUDENT = "student"
     COLLECTION_CONFIG = "connect"
     FIELD_NAME = "key"
